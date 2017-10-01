bdc7b99779a35b087b563379623a75171889851f
hongxiang <hx.hongxiang@gmail.com>
2017-10-02 00:54:48 +0800

    update

---
+++ dxpy/task/database_interface.py
@@ -0,0 +1,28 @@
+def read(tid):
+    pass
+
+
+def read_all():
+    pass
+
+
+def update(task):
+    pass
+
+
+def delete(tid):
+    pass
+
+
+class Http:
+    @classmethod
+    def read(cls, tid):
+        pass
+
+    @classmethod
+    def read_all(cls):
+        pass
+
+    @classmethod
+    def update(task):
+        pass
---
+++ dxpy/task/zoo/sleep_chain.py
@@ -0,0 +1,23 @@
+from dxpy.file_system.path import Path
+from ..representation.factory import create_task_graph
+from ..representation.templates import TaskCommand
+from ..interface import create_graph
+
+
+class SleepChainCreator:
+    def __init__(self, workdir=None, duration=5):
+        self.workdir = Path(workdir)
+        self.duration = duration
+
+    def create(self):
+        cmd = 'sleep {0} && hostname'
+        t1 = TaskCommand(cmd.format(self.duration, 1), workdir=self.workdir)
+        t2 = TaskCommand(cmd.format(self.duration, 2), workdir=self.workdir)
+        t3 = TaskCommand(cmd.format(self.duration, 3), workdir=self.workdir)
+        g = create_task_graph([t1, t2, t3], [None, 0, 1])
+        return g
+
+
+class SbatchCreator:
+    def __init__(self, workdir=None, ):
+        pass
