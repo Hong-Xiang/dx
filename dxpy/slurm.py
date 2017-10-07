ab432ed9866fb7a76d4828a7ea39bf140bd50cb8
HongXiang <hx.hongxiang@gmail.com>
2017-10-07 10:08:01 +0800

    slurm tests added

--- dxpy/slurm.py
+++ dxpy/slurm.py
@@ -28,44 +28,27 @@ class TaskInfo:
         else:
             return cls(*items)
 
-# class Slurm:
-#     def __init__(self):
-#         self._sinfo_buffer = None
 
-#     def _assign_sinfo(self, value):
-#         self._sinfo_buffer = value
+def apply_command(command):
+    with os.popen(command) as fin:
+        return fin.readlines()
 
-#     def _sinfo_kernel(self):
-#         (Observable.from_(os.popen('squeue').readlines())
-#          .map(lambda l: re.sub('\s+', ' ', l).strip())
-#          .map(lambda l: l.split(' '))
-#          .filter(lambda l: l[0].isdigit())
-#          .map(lambda s: [int(s[0])] + s[1:])
-#          .map(lambda s: SINFO(*s))
-#          .to_list()
-#          .subscribe(lambda s: self._assign_sinfo(s)))
-
-#     def sinfo(self):
-#         self._sinfo_kernel()
-#         if self._sinfo_buffer is None:
-#             self._sinfo_buffer = []
-#         return self._sinfo_buffer
-
-#     def srun(command):
-#         with os.popen(command) as fin:
-#             return fin.readlines()
 
 def sbatch(workdir, script_file):
-    with os.popen('cd {dir} && sbatch {file}'.format(dir=workdir, file=script_file)) as fin:
-        return sid_from_submit(fin.readlines()[0])
+    result = apply_command('cd {dir} && sbatch {file}'.format(
+        dir=workdir, file=script_file))
+    return sid_from_submit(result[0])
+
 
 def sid_from_submit(s):
     return int(re.sub('\s+', ' ', s).strip().split(' ')[3])
 
+
 def squeue():
-    return (Observable.from_(os.popen('squeue').readlines())
+    return (Observable.from_(apply_command('squeue'))
             .map(lambda l: TaskInfo.from_squeue(l))
             .filter(lambda l: l is not None))
+
 
 def is_complete(sid):
     return (squeue()
