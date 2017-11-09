bac16f8c291d549a6170f9f2ba52be0e656511f5
HongXiang <hx.hongxiang@gmail.com>
2017-11-09 09:54:53 +0800

    enhanced grid view with adjust width, heigth and padding

--- dxpy/dxpy/numpy_extension/visual/image.py
+++ dxpy/dxpy/numpy_extension/visual/image.py
@@ -3,22 +3,42 @@ import numpy as np
 from .. import image as nei
 
 
-def grid_view(image_lists, windows=None, nb_column=8, scale=2.0, cmap=None, *, hide_axis=False, tight_c=0.3, return_figure=False):
+def grid_view(image_lists, windows=None, nb_column=8, scale=1.0, cmap=None, *, hide_axis=False, tight_c=0.001, return_figure=False, dpi=50, adjust_figure_size=True):
     """ subplot list of images of multiple categories into grid subplots
     Args:
         image_lists: list of [list of images or 4D tensor]
         windows: list of windows
         nb_column: columns of images
+    Returns:
+        Return figure if return_figure is true, else None.
     """
+    from dxpy.debug import dbgmsg
+    dbgmsg("V", 0.1)
     nb_cata = len(image_lists)
     if windows is None:
         windows = [(None, None)] * nb_cata
-    image_lists = map(nei.try_unbatch, image_lists)
+    image_lists = list(map(nei.try_unbatch, image_lists))
+    for i, v in enumerate(image_lists):
+        if not isinstance(v, list) and isinstance(v, np.ndarray) and v.ndim == 2:
+            image_lists[i] = [v]
     image_lists = [list(map(nei.fix_dim, imgs)) for imgs in image_lists]
+    dbgmsg(image_lists[0][0])
     nb_images = max([len(imgs) for imgs in image_lists])
 
     nb_row = np.ceil(nb_images / nb_column) * nb_cata
-    fig = plt.figure(figsize=(nb_column * scale, nb_row * scale))
+
+    def adjust_figure_size():
+        width = nb_column * scale
+        height = nb_row * scale
+        if adjust_figure_size:
+            shapes = [(il[0].shape[1], il[0].shape[0])
+                      for il in image_lists if len(il) > 0]
+            w, h = zip(*shapes)
+            width *= np.mean(w) / 100
+            height *= np.mean(h) / 100
+        return (width, height)
+    width, height = adjust_figure_size()
+    fig = plt.figure(figsize=(width, height), dpi=dpi)
 
     for k in range(nb_cata):
         for i in range(nb_images):
@@ -32,6 +52,11 @@ def grid_view(image_lists, windows=None, nb_column=8, scale=2.0, cmap=None, *, h
             if hide_axis:
                 ax.get_xaxis().set_visible(False)
                 ax.get_yaxis().set_visible(False)
-    plt.tight_layout(tight_c)
+    if isinstance(tight_c, list):
+        h_pad, w_pad = tight_c
+    else:
+        h_pad = tight_c
+        w_pad = tight_c
+    plt.tight_layout(h_pad=h_pad, w_pad=w_pad)
     if return_figure:
         return fig
