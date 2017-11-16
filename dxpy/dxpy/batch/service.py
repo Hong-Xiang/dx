2db88311505d5a624c8d9034910b2e7816388925
HongXiang <hx.hongxiang@gmail.com>
2017-11-16 20:03:22 +0800

    add batch fetch command

--- dxpy/dxpy/batch/service.py
+++ dxpy/dxpy/batch/service.py
@@ -3,6 +3,9 @@ from fs import copy, path
 
 
 def files_in_directories(fs, include_dirs, include_files, depth=0, exclude_dirs=None, exclude_files=None):
+    """
+    Args:
+    """
     from .model.filters import FilesFilter, DirectoriesFilter
     if not isinstance(include_dirs, (list, tuple)):
         raise TypeError(
@@ -10,7 +13,7 @@ def files_in_directories(fs, include_dirs, include_files, depth=0, exclude_dirs=
     if not isinstance(include_files, (list, tuple)):
         raise TypeError(
             'include_dirs should be list or tuple of str, not {}.'.format(type(include_files)))
-    results = list()
+    results = list()    
     (DirectoriesFilter(include_dirs, exclude_dirs, depth).obv(fs)
      .flat_map(lambda p: FilesFilter(include_files, exclude_files).obv(fs, p))
      .subscribe(results.append))
