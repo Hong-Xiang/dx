519cacb3e8f0cb284a135abd175040e831be640f
HongXiang <hx.hongxiang@gmail.com>
2017-11-22 14:55:21 +0800

    mnist still work

--- dxpy/dxpy/learn/dataset/mnist/dataapi.py
+++ dxpy/dxpy/learn/dataset/mnist/dataapi.py
@@ -3,20 +3,11 @@ from ..base import DatasetTFRecords, NodeKeys
 
 
 class MNISTTFRecords(DatasetTFRecords):
-    def __init__(self, name='dataset', **config):
-        super(__class__, self).__init__(name, **config)
-
-    # def _pre_processing(self):
-    #     self._add_normalizer()
-
-    # def _add_normalizer(self):
-    #     from ...model.normalizer import get_normalizer
-    #     from ...model.tensor import PlaceHolder
-    #     self._normalizer = None
-    #     normalizer_class = get_normalizer(self.c['normalization']['method'])
-    #     if normalizer_class is not None:
-    #         self._normalizer = normalizer_class(self.name / 'normalization',
-    #                                             PlaceHolder([28, 28, 1]), lazy_create=True)
+    def __init__(self, name='dataset', *, files=None, batch_size=None, one_hot=None, **config):
+        super(__class__, self).__init__(name, files=files,
+                                        batch_size=batch_size,
+                                        one_hot=one_hot,
+                                        **config)
 
     @classmethod
     def _default_config(cls):
@@ -87,72 +78,72 @@ class MNISTTFRecords(DatasetTFRecords):
                 .repeat())
 
 
-from ..base import Graph
+# from ..base import Graph
 
 
-class MNISTLoadAll(Graph):
-    def __init__(self, name='/dataset', **config):
-        # from ..preprocessing.normalizer import SelfMinMax
+# class MNISTLoadAll(Graph):
+#     def __init__(self, name='/dataset', **config):
+#         # from ..preprocessing.normalizer import SelfMinMax
 
-        super(__class__, self).__init__(name, **config)
-        self._normalizer = None
-        method = self.c['normalization']['method'].lower()
-        if method != 'pass':
-            self._normalizer = get_normalizer(
-                method)(self.name / 'normalization', )
-        self.register_main_node(self.__load_data())
-        self.register_node('image', self.as_tensor()['image'])
-        self.register_node('label', self.as_tensor()['label'])
+#         super(__class__, self).__init__(name, **config)
+#         self._normalizer = None
+#         method = self.c['normalization']['method'].lower()
+#         if method != 'pass':
+#             self._normalizer = get_normalizer(
+#                 method)(self.name / 'normalization', )
+#         self.register_main_node(self.__load_data())
+#         self.register_node('image', self.as_tensor()['image'])
+#         self.register_node('label', self.as_tensor()['label'])
 
-    def get_feed_dict(self):
-        return tf.get_default_session().run(
-            {'image': self.tensor('image'), 'label': self.tensor('label')})
+#     def get_feed_dict(self):
+#         return tf.get_default_session().run(
+#             {'image': self.tensor('image'), 'label': self.tensor('label')})
 
-    @classmethod
-    def _default_config(cls):
-        return {
-            'batch_size': 32,
-        }
+#     @classmethod
+#     def _default_config(cls):
+#         return {
+#             'batch_size': 32,
+#         }
 
-    def post_session_created(self):
-        tf.get_default_session().run(self._iter_init,
-                                     feed_dict={
-                                         self.tensor('images'): self.images,
-                                         self.tensor('labels'): self.labels
-                                     })
+#     def post_session_created(self):
+#         tf.get_default_session().run(self._iter_init,
+#                                      feed_dict={
+#                                          self.tensor('images'): self.images,
+#                                          self.tensor('labels'): self.labels
+#                                      })
 
-    def _normalize(self, image):
-        if self._normalizer is None:
-            return image
-        else:
-            return self._normalizer(image)['data']
+#     def _normalize(self, image):
+#         if self._normalizer is None:
+#             return image
+#         else:
+#             return self._normalizer(image)['data']
 
-    def __load_data(self):
-        from tensorflow.examples.tutorials.mnist import input_data
-        from tensorflow.contrib.data import Dataset
-        mnist = input_data.read_data_sets(
-            '/home/hongxwing/Datas/mnist/tfdefault', one_hot=True)
-        self.images = mnist.train.images
-        self.labels = mnist.train.labels
+#     def __load_data(self):
+#         from tensorflow.examples.tutorials.mnist import input_data
+#         from tensorflow.contrib.data import Dataset
+#         mnist = input_data.read_data_sets(
+#             '/home/hongxwing/Datas/mnist/tfdefault', one_hot=True)
+#         self.images = mnist.train.images
+#         self.labels = mnist.train.labels
 
-        # def gen_image():
-        #     for i in range(images.shape[0]):
-        #         yield {'image': images[i, ...], 'label': label[i, ...]}
-        with tf.name_scope(self.basename):
-            images_tensor = tf.placeholder(tf.float32, self.images.shape,
-                                           'mnist_images')
-            self.register_node('images', images_tensor)
-            labels_tensor = tf.placeholder(tf.int64, self.labels.shape,
-                                           'mnist_label')
-            self.register_node('labels', labels_tensor)
-            dataset_image = (Dataset.from_tensor_slices(images_tensor)
-                             .map(lambda img: tf.reshape(img, [28, 28, 1]))
-                             .map(self._normalize))
-            dataset_label = Dataset.from_tensor_slices(labels_tensor)
-            dataset = Dataset.zip(
-                {'image': dataset_image, 'label': dataset_label})
-            dataset = dataset.batch(self.param('batch_size')).repeat()
-            iterator = dataset.make_initializable_iterator()
-            self._iter_init = iterator.initializer
-            next_element = iterator.get_next()
-        return next_element
+#         # def gen_image():
+#         #     for i in range(images.shape[0]):
+#         #         yield {'image': images[i, ...], 'label': label[i, ...]}
+#         with tf.name_scope(self.basename):
+#             images_tensor = tf.placeholder(tf.float32, self.images.shape,
+#                                            'mnist_images')
+#             self.register_node('images', images_tensor)
+#             labels_tensor = tf.placeholder(tf.int64, self.labels.shape,
+#                                            'mnist_label')
+#             self.register_node('labels', labels_tensor)
+#             dataset_image = (Dataset.from_tensor_slices(images_tensor)
+#                              .map(lambda img: tf.reshape(img, [28, 28, 1]))
+#                              .map(self._normalize))
+#             dataset_label = Dataset.from_tensor_slices(labels_tensor)
+#             dataset = Dataset.zip(
+#                 {'image': dataset_image, 'label': dataset_label})
+#             dataset = dataset.batch(self.param('batch_size')).repeat()
+#             iterator = dataset.make_initializable_iterator()
+#             self._iter_init = iterator.initializer
+#             next_element = iterator.get_next()
+#         return next_element
