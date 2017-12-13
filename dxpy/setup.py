5d9155f5eada9f36ffb629599b484de44b98282c
HongXiang <hx.hongxiang@gmail.com>
2017-12-13 20:27:18 +0800

    add dataest dist support

--- dxpy/setup.py
+++ dxpy/setup.py
@@ -1,6 +1,6 @@
 from setuptools import setup, find_packages
 setup(name='dxl-dxpy',
-      version='0.7.8',
+      version='0.7.9',
       description='Duplicate components library python library.',
       url='https://github.com/Hong-Xiang/dxl',
       author='Hong Xiang',
