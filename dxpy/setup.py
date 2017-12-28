bb7396e57ca5169a9d635ad02083de6aeefc6916
HongXiang <hx.hongxiang@gmail.com>
2017-12-28 14:46:08 +0800

    mCT ready

--- dxpy/setup.py
+++ dxpy/setup.py
@@ -1,6 +1,6 @@
 from setuptools import setup, find_packages
 setup(name='dxl-dxpy',
-      version='0.7.27',
+      version='0.7.29',
       description='Duplicate components library python library.',
       url='https://github.com/Hong-Xiang/dxl',
       author='Hong Xiang',
