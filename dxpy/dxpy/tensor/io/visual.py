b15e4123163d174e6fcd45d131e66113fb414e5c
HongXiang <hx.hongxiang@gmail.com>
2018-02-21 20:43:27 +0800

    check point before incident estimation project

--- dxpy/dxpy/tensor/io/visual.py
+++ dxpy/dxpy/tensor/io/visual.py
@@ -219,6 +219,23 @@ def grid_view(images, windows=None, scale=1.0, cmap=None,
         return fig
 
 
+
+_text_configs = dict()
+_image_configs = dict()
+
+class PlotConfigContent:
+    _config_dict = None
+    _target_dict = None
+    def __init__(self, cfg=None):
+        if cfg is not None:
+            self._pre_config_dict = self._config_dict
+            self._config_dict = cfg
+
+    def __enter__(self):
+        pass
+
+        
+
 class PlotItem:
     def __init__(self, start, span, content, axis_type='line_bold', axis_color='k', fc='w', **kw):
         self._s = start
@@ -269,7 +286,7 @@ class TextItem(PlotItem):
 
 
 class ColorBarItem(PlotItem):
-    def __init__(self, start, span, img_item, image_scale=256.0 * 1.414, line_scale=100.0, label="10 mm", font_size=16, line_width=3.0, font_color='k', **kw):
+    def __init__(self, start, span, img_item, image_scale=256.0 * 1.414, line_scale=100.0, label="10 mm", font_size=16, line_width=3.0, font_color='k', no_line=False, cbar_range=None, force_int=True, **kw):
         super().__init__(start, span, img_item, **kw)
         self._image_scale = image_scale
         self._line_scale = line_scale
@@ -277,6 +294,9 @@ class ColorBarItem(PlotItem):
         self._fontsize = font_size
         self._fontcolor = font_color
         self._line_width = line_width
+        self._no_line = no_line
+        self._cbar_range = cbar_range
+        self._force_int = force_int
 
     def _get_cbar_axes(self, fig, ax):
         bbox = ax.get_position()
@@ -310,10 +330,22 @@ class ColorBarItem(PlotItem):
     def _add_colorbar(self, fig, ax):
         import matplotlib.pyplot as plt
         import numpy as np
-        vmin, vmax = self._c._img.get_clim() 
-        tks = np.linspace(vmin, vmax, 4).flatten()
-        tks = ['{:3.0f}'.format(v) for v in tks]
-        tks = [float(v) for v in tks]
+        if self._cbar_range is None:
+            vmin, vmax = self._c._img.get_clim() 
+        else:
+            vmin, vmax = self._cbar_range
+        if self._force_int:
+            tks = np.linspace(vmin, vmax, 4).flatten()
+            tks = ['{:3.0f}'.format(v) for v in tks]
+            tks = [float(v) for v in tks]
+            if tks[-1] > vmax:
+                tks[-1] = vmax - (vmax-vmin)*0.05
+                tks[-1] = '{:3.0f}'.format(tks[-1])
+                tks[-1] = float(tks[-1])
+        else:
+            tks = np.linspace(vmin, vmax, 3).flatten()
+            tks = ['{:0.1f}'.format(v) for v in tks]
+            tks = [float(v) for v in tks]
         cb = plt.colorbar(self._c._img, self._get_cbar_axes(fig, ax), ticks=tks)
         cb.ax.tick_params(labelsize=self._fontsize)
         cb.ax.tick_params(labelcolor=self._fontcolor)
@@ -321,7 +353,8 @@ class ColorBarItem(PlotItem):
         import matplotlib.pyplot as plt
         ax = super().add_to_figure(fig, gs)
         self._add_colorbar(fig, ax)
-        self._add_line(ax)
+        if not self._no_line:
+            self._add_line(ax)
         return ax
 
 
@@ -339,6 +372,31 @@ class ImageItem(PlotItem):
             vmin, vmax = self._window
         self._img = ax.imshow(self._c, cmap=self._cmap,
                               aspect='auto', vmin=vmin, vmax=vmax)
+        return ax
+
+class ImageZoomItem(ImageItem):
+    def __init__(self, slicer, grids=(3,3), *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self._slicer = slicer
+        self._grids = grids
+
+    def _get_mini_img_ax(self, fig, ax, irow, icol):        
+        bbox = ax.get_position()
+        l, b, r, t = bbox.x0, bbox.y0, bbox.x1, bbox.y1
+        sx, sy = (r-l) / self._grids[1], (t-b)/ self._grids[0]
+        return fig.add_axes([l+sx*icol, b+sy*irow,sx,sy])
+    
+    def _add_mini_imgs(self, fig, ax):
+        mini_imgs = self._slicer(self._c)
+        for i, row in enumerate(mini_imgs):
+            for j, v in enumerate(row):
+                if v is None:
+                    continue
+                ax_mini = self._get_mini_img_ax(fig, ax)
+                ax.imshow(v, cmap=self._cmap,
+                          aspect='auto', vmin=vmin, vmax=vmax)
+    def add_to_figure(self, fig, gs):
+        ax = super().add_to_figure(fig, gs)
         return ax
 
 
